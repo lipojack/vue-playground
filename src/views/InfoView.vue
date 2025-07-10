8eb6f6c8e5984de138c7a9e52540a5e5a38181ca
lipojack <cp000123@gmail.com>
2025-07-10 23:56:18 +0800

    Adopted naive ui, cleant up files

---
+++ src/views/InfoView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import InfoTable from '@/components/InfoTable.vue';
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+      <InfoTable />
+    </div>
+  </header>
+
+</template>
+
