6f450fbb1cac1303f34ce86b0af56bbf7f25a906
lipojack <cp000123@gmail.com>
2025-07-12 03:47:29 +0800

    Sticked header, scrollable sidebar, main

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,29 @@ import { NConfigProvider } from 'naive-ui'
 </script>
 
 <template>
-  <!-- <n-config-provider> -->
-    <div class="bg-blue-500 text-white p-4">
-    Test Tailwind BG
-  </div>
-    <div class="flex flex-col h-screen bg-red-100">
-      <!-- Sticky Header -->
-      <header
-        class="sticky top-0 z-50 p-4 shadow bg-red-50"
-      >
-        <h1 class="text-xl font-semibold">Dashboard Header</h1>
-      </header>
+  <div class="flex flex-col h-screen bg-red-100">
+    <!-- Sticky Header -->
+    <header
+      class="sticky top-0 z-50 p-4 shadow bg-blue-100"
+    >
+      <h1 class="text-xl font-semibold">Dashboard Header</h1>
+    </header>
 
-      <div class="flex ">
-      <!-- Sidebar -->
-        <aside class="w-128 bg-gray-100 p-4 overflow-y-auto h-screen">
-          <nav class="flex flex-col space-y-2">
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink to="/info">Info</RouterLink>
-          </nav>
-        </aside>
+    <div class="flex flex-1 overflow-hidden">
+    <!-- Sidebar -->
+      <aside class="flex-1 w-128 bg-gray-20 p-4 overflow-y-auto h-screen">
+        <nav class="flex flex-col space-y-2">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/info">Info</RouterLink>
+        </nav>
+      </aside>
 
-        <!-- Main Content -->
-        <main class="overflow-y-auto p-6 bg-gray-50">
-          <RouterView />
-        </main>
-      </div>
+      <!-- Main Content -->
+      <main class="flex-4 overflow-y-auto p-6 bg-green-50">
+        <RouterView />
+      </main>
     </div>
-  <!-- </n-config-provider> -->
+  </div>
 
 </template>
